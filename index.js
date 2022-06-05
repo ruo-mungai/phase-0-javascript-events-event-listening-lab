499fe5be1dd8ef5fe3a37b1fac81a5cf34081e3a
Patrick <ruomungai@gmail.com>
2022-06-05 09:18:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(){alert('I was clicked')});
+}
+
+//callback: arrow function
+function addingEventListenerArr(){
+    const input = document.getElementById('input');
+    input.addEventListener('click', () => alert('I was clicked'));
 }
